3a26f9ed1402e2fdfc5974c6b61cafe2927750ad
Freja <frej400d@stud.kea.dk>
2023-02-19 16:49:35 +0100

    base layout

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -5,11 +5,26 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': '500px',
-        'md': '987px',
-        'lg': '1265px',
+        sm: "500px",
+        md: "987px",
+        lg: "1265px",
+        xl: "1500px",
+        xxl: "1800px",
+      },
+      gridTemplateColumns: {
+        layout_mobile: "68px 1fr",
+        layout: "300px 1fr",
+      },
+      width: {
+        nav_mobile: "68px",
+        nav: "600px",
+      },
+      maxWidth: {
+        layout: "1265px",
+        feed: "600px",
+        widget: "350px",
       },
     },
-    },
+  },
   plugins: [],
-}
+};
