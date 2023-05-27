98a09dbffe26d68314fa89484a0200140b2caf5d
Freja <frej400d@stud.kea.dk>
2023-05-27 14:23:19 +0200

    email verification done

--- apis/api_verify_email.py
+++ apis/api_verify_email.py
@@ -7,7 +7,7 @@ def _():
     try:
         #Get api key from form
         user_api_key = request.forms.get("welcome_user_api_key")
-        if not user_api_key: raise Exception("Something went wrong")
+        if not user_api_key: raise Exception(400, "Missing request body")
         
         #Open database
         db = x.db()
@@ -16,26 +16,27 @@ def _():
         user = db.execute("SELECT * FROM users WHERE user_api_key=? COLLATE NOCASE", (user_api_key,)).fetchone()
 
         #The api key does not exists, it is either wrong or has been changed/updated
-        if not user: raise Exception("An error occurred")
-
+        if not user: raise Exception(400, "An error occurred")
+        
+        #User id
+        user_id = user["user_id"]
+        
         #Update account status to active
         db.execute(f"UPDATE users SET user_account_status=? WHERE user_api_key=?", (x.ACC_STATUS_ACTIVE, user_api_key,))
 
-        #throw exception to test rollback
-
-        #create new api key
+        #New api key
         new_api_key = str(uuid.uuid4().hex)
 
-        #Update updated at
+        #Assign new api key
+        db.execute(f"UPDATE users SET user_api_key=? WHERE user_id=?", (new_api_key, user_id))
 
-
-        #commit
+        #Commit
         db.commit()
 
         return {"info":"ok"}
     except Exception as ex:
         print(ex)
-        #rollback
+        if "db" in locals(): db.rollback()
         try:
             response.status = ex.args[0]
             return {"info":ex.args[1]}
